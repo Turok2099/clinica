2025a1d638b3257f85f0bb32b904b54b0a0721ef
turok2099 <jorge.castro.cruz@hotmail.com>
2026-06-06 23:17:13 -0600

    actualizacion de landing

--- app/alt/page.tsx
+++
@@ -1,9 +0,0 @@
-import HomeView from '@/views/home-alt';
-
-export default function AltHome() {
-  return (
-    <main className="theme-alt min-h-screen bg-[#FDF8F2]">
-      <HomeView />
-    </main>
-  );
-}
